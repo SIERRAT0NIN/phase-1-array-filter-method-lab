60785e1fc0b5e7fb8880869563a4d14af9e614f7
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-09-13 18:58:53 -0700

    completed array filter

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+const findMatching = (drivers, nameToMatch) => {
+  const result = drivers.filter(
+    (driver) => driver.toLowerCase() === nameToMatch.toLowerCase()
+  );
+  return result;
+};
+const fuzzyMatch = (drivers, letters) =>
+  drivers.filter((driver) => driver.startsWith(letters));
+
+const matchName = (drivers, name) =>
+  drivers.filter((driver) => driver.name === name);
+
+matchName();
